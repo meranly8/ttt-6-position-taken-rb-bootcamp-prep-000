9084e9cfec907748ff795b61592fab6a3d881d26
Michelle Ranly <meranly8@gmail.com>
2020-01-20 17:48:09 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == " " || "" || NIL
+  if board[index] == " " || board[index == ]"" || board[index == ]NIL
+    return false
+  else
+    return true
   end
 end
